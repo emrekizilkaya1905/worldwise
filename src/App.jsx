c9a572384d2fdc6c74fc49acb7daef7d73520c55
emrekizilkaya1905 <144436310+emrekizilkaya1905@users.noreply.github.com>
2025-04-19 23:52:07 +0200

    Link routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
   return (
-    <div>
-      <h2>Emre</h2>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Pricing.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function Pricing() {
+  return (
+    <div>
+      <PageNav />
+      Price
+    </div>
+  );
+}
+
+export default Pricing;
