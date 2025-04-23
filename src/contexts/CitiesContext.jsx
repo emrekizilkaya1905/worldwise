d8e9e068069d9485395137db45888d513d9d55f5
emrekizilkaya1905 <144436310+emrekizilkaya1905@users.noreply.github.com>
2025-04-23 17:31:01 +0200

    Install leaflet

---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,55 @@
+import { createContext, useState, useEffect, useContext } from "react";
+
+const CitiesContext = createContext();
+const BASE_URL = "http://localhost:9000";
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("Something went wrong");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch {
+      alert("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("You are not using context in right way.");
+  return context;
+}
+export { CitiesProvider, useCities };
